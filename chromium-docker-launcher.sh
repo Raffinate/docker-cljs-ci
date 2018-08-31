254ff02611d03d51886170e58ed394cf5264278d
Aleksey Mokhovikov <moxobukob@gmail.com>
2018-08-31 23:25:53 +0700

    Added script to start chromium in container

---
+++ chromium-docker-launcher.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+Xvfb :0 -screen 0 1920x1080x24 -ac +extension GLX +render -noreset &
+# no-sandbox option may be omited in case user is properly set up in container
+# see https://developers.google.com/web/updates/2017/04/headless-chrome
+# and https://github.com/ebidel/lighthouse-ci/blob/master/builder/Dockerfile#L35-L40
+DISPLAY=:0.0 chromium-browser --disable-gpu --no-sandbox $@
+killall Xvfb
